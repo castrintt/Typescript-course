d0e4c41f2fab52dc2de7f008ab546667cb375b5d
castro <igor.castro@xbits.com.br>
2022-08-10 23:22:10 -0300

    ICA public, private and protected

--- classesComTs/index.js
+++ classesComTs/index.js
@@ -83,3 +83,124 @@ console.log(destroyer.kill());
 console.log(destroyer.kill());
 console.log(destroyer.kill());
 console.log(destroyer.kill());
+//                              o this
+class Truck {
+    constructor(model, hp) {
+        this.model = model;
+        this.hp = hp;
+    }
+    showDetails() {
+        console.log(`Caminhão do modelo: ${this.model}, que tem ${this.hp} cavalos de potencia`);
+    }
+}
+const myTruck = new Truck("Scania", 4000);
+myTruck.showDetails();
+//                                    Getters
+class Character {
+    constructor(name, power) {
+        this.__name = name;
+        this.__power = power;
+    }
+    get name() {
+        return `My name is ${this.__name}`;
+    }
+    set CharacterName(otherName) {
+        this.__name = otherName;
+    }
+}
+const geralt = new Character("Geralt de Rivia", 100);
+console.log(geralt.name);
+geralt.CharacterName = "Igor de RiverWood";
+console.log(geralt.name);
+//                          setters
+class Coords {
+    get showX() {
+        return this.__x;
+    }
+    get showY() {
+        return this.__y;
+    }
+    set setX(newX) {
+        this.__x = newX;
+    }
+    set setY(newY) {
+        this.__y = newY;
+    }
+}
+const coordenadas = new Coords();
+coordenadas.setX = 30;
+coordenadas.setY = 40;
+console.log(coordenadas.showX);
+console.log(coordenadas.showY);
+class BlogPost {
+    constructor(title) {
+        this.title = title;
+    }
+    itemTitle() {
+        return `o titulo do post é ${this.title}`;
+    }
+}
+const newPost = new BlogPost("aprendendo typescript");
+console.log(newPost.itemTitle());
+//                                override
+class Base {
+    someMethod() {
+        console.log("alguma coisa");
+    }
+}
+class Nova extends Base {
+    someMethod() {
+        console.log("alterado");
+    }
+}
+const myNewObject = new Base();
+const myObject = new Nova();
+myNewObject.someMethod();
+myObject.someMethod();
+//                        visibilidade
+// public, protected  e private
+//public
+class Food {
+    constructor(meat, drink) {
+        this.meat = meat;
+        this.drink = drink;
+        this.meat = meat;
+        this.drink = drink;
+    }
+}
+class SubClassFood extends Food {
+    constructor(meat, drink) {
+        super(meat, drink);
+        this.meat = meat;
+        this.drink = drink;
+    }
+}
+const meal = new Food("Carne", "Água");
+const newMeal = new SubClassFood("Carne", "Refri");
+console.log(meal);
+console.log(newMeal);
+// protected
+class E {
+    constructor() {
+        this.x = 10;
+    }
+    someMethod() {
+        console.log("esse metodo é protegido! só pode ser acessado por outro metodo");
+    }
+}
+class F extends E {
+    get showX() {
+        return this.x;
+    }
+    get showProtectedMethod() {
+        return this.someMethod();
+    }
+    set changeX(newX) {
+        this.x = newX;
+    }
+}
+const fInstance = new F();
+console.log(fInstance.showX); //somente com um metodo (nesse caso um get que podemos acessar as o valor da classe)
+fInstance.changeX = 40; // só podemos alterar a propriedade por meio de um metodo tbm (nesse caso um setter)
+console.log(fInstance.showX);
+fInstance.showProtectedMethod;
--- classesComTs/index.ts
+++ classesComTs/index.ts
@@ -125,3 +125,181 @@ console.log(destroyer.kill());
 console.log(destroyer.kill());
 console.log(destroyer.kill());
 console.log(destroyer.kill());
+
+//                              o this
+
+class Truck {
+  model;
+  hp;
+
+  constructor(model: string, hp: number) {
+    this.model = model;
+    this.hp = hp;
+  }
+
+  showDetails() {
+    console.log(
+      `Caminhão do modelo: ${this.model}, que tem ${this.hp} cavalos de potencia`
+    );
+  }
+}
+
+const myTruck = new Truck("Scania", 4000);
+
+myTruck.showDetails();
+
+//                                    Getters
+
+class Character {
+  __name;
+  __power;
+
+  constructor(name: string, power: number) {
+    this.__name = name;
+    this.__power = power;
+  }
+
+  get name(): string {
+    return `My name is ${this.__name}`;
+  }
+
+  set CharacterName(otherName: string) {
+    this.__name = otherName;
+  }
+}
+
+const geralt = new Character("Geralt de Rivia", 100);
+
+console.log(geralt.name);
+
+geralt.CharacterName = "Igor de RiverWood";
+
+console.log(geralt.name);
+
+//                          setters
+
+class Coords {
+  __x!: number;
+  __y!: number;
+
+  get showX() {
+    return this.__x;
+  }
+  get showY() {
+    return this.__y;
+  }
+
+  set setX(newX: number) {
+    this.__x = newX;
+  }
+  set setY(newY: number) {
+    this.__y = newY;
+  }
+}
+
+const coordenadas = new Coords();
+coordenadas.setX = 30;
+coordenadas.setY = 40;
+
+console.log(coordenadas.showX);
+console.log(coordenadas.showY);
+
+//                        herança de interfaces
+
+interface showTitle {
+  itemTitle(): string;
+}
+
+class BlogPost implements showTitle {
+  title;
+  constructor(title: string) {
+    this.title = title;
+  }
+
+  itemTitle(): string {
+    return `o titulo do post é ${this.title}`;
+  }
+}
+
+const newPost = new BlogPost("aprendendo typescript");
+
+console.log(newPost.itemTitle());
+
+//                                override
+
+class Base {
+  someMethod(): void {
+    console.log("alguma coisa");
+  }
+}
+
+class Nova extends Base {
+  someMethod(): void {
+    console.log("alterado");
+  }
+}
+
+const myNewObject = new Base();
+const myObject = new Nova();
+
+myNewObject.someMethod();
+myObject.someMethod();
+
+//                        visibilidade
+
+// public, protected  e private
+
+//public
+
+class Food {
+  constructor(public meat: string, public drink: string) {
+    this.meat = meat;
+    this.drink = drink;
+  }
+}
+
+class SubClassFood extends Food {
+  constructor(public meat: string, public drink: string) {
+    super(meat, drink);
+  }
+}
+
+const meal = new Food("Carne", "Água");
+const newMeal = new SubClassFood("Carne", "Refri");
+
+console.log(meal);
+console.log(newMeal);
+
+// protected
+
+class E {
+  protected x = 10;
+  protected someMethod() {
+    console.log(
+      "esse metodo é protegido! só pode ser acessado por outro metodo"
+    );
+  }
+}
+
+class F extends E {
+  get showX(): number {
+    return this.x;
+  }
+
+  get showProtectedMethod() {
+    return this.someMethod();
+  }
+
+  set changeX(newX: number) {
+    this.x = newX;
+  }
+}
+
+const fInstance = new F();
+
+console.log(fInstance.showX); //somente com um metodo (nesse caso um get que podemos acessar as o valor da classe)
+
+fInstance.changeX = 40; // só podemos alterar a propriedade por meio de um metodo tbm (nesse caso um setter)
+
+console.log(fInstance.showX);
+fInstance.showProtectedMethod;
